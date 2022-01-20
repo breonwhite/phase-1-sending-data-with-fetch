02f6a69a60f3d1e76d4983032f1b51333594bdf8
Breon White <bdwhite92@gmail.com>
2022-01-20 01:16:27 -0500

    commit tree cdb398c85a5665f00ef50620325809f4ed155060

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const newBody = document.getElementsByTagName("body")[0];
+
+function submitData(userName, userEmail){
+    const login = {
+        "name": userName,
+        "email": userEmail
+    }
+    return fetch(`http://localhost:3000/users`,{
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+       body: JSON.stringify(login),
+    })
+    .then(res => res.json())
+    .then(Object => newBody.append(Object.id))
+    
+    .catch(function (error) {
+        const alertMessage = error.message;
+        document.body.innerHTML = `${alertMessage}`;
+    })
+};
